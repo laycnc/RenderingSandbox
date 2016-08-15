d50dda12bc649a9cfef57467cb48b459ff2b8a35
st <st@DESKTOP-KUT9OLE>
2016-08-16 01:08:41 +0900

    初期プロジェクトの作成

---
+++ RenderingSandbox/RenderingSandbox/RenderingSandboxMain.h
@@ -0,0 +1,36 @@
+﻿#pragma once
+
+#include <memory>
+#include "Common\StepTimer.h"
+#include "Common\DeviceResources.h"
+#include "Content\Sample3DSceneRenderer.h"
+#include "Content\SampleFpsTextRenderer.h"
+
+// Renders Direct2D and 3D content on the screen.
+namespace RenderingSandbox
+{
+	class RenderingSandboxMain : public DX::IDeviceNotify
+	{
+	public:
+		RenderingSandboxMain(const std::shared_ptr<DX::DeviceResources>& deviceResources);
+		~RenderingSandboxMain();
+		void CreateWindowSizeDependentResources();
+		void Update();
+		bool Render();
+
+		// IDeviceNotify
+		virtual void OnDeviceLost();
+		virtual void OnDeviceRestored();
+
+	private:
+		// Cached pointer to device resources.
+		std::shared_ptr<DX::DeviceResources> m_deviceResources;
+
+		// TODO: Replace with your own content renderers.
+		std::unique_ptr<Sample3DSceneRenderer> m_sceneRenderer;
+		std::unique_ptr<SampleFpsTextRenderer> m_fpsTextRenderer;
+
+		// Rendering loop timer.
+		DX::StepTimer m_timer;
+	};
+}
